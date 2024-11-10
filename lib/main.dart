c8c8fb8c5a97b6b6df72e69ca66faa17c9ae64a3
hasan-elbadru <hasan.elbadry2006@gmail.com>
2024-11-10 09:17:19 +0200

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) => MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
           useMaterial3: true,
         ),
         initialRoute: SignUp.route,
